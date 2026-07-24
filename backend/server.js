cf932ae734dbba0d82b7eca6a72d48a69b02d927
kabirou koulika <kabiroukoulika@gmail.com>
2026-07-25 00:46:22 +0100

    bref

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(helmet());
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://oilai-invest.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean)
 
